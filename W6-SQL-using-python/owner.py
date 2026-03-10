af73a3105495f7255d6a6c3918dd85b707f675f8
Tommi Kyllönen <tommi.kyllonen@gmail.com>
2026-03-10 22:08:46 +0200

    almost end of the tutorial

--- W6-SQL-using-python/owner.py
+++ W6-SQL-using-python/owner.py
@@ -2,7 +2,7 @@ class Owner():
     def __init__(self, fname: str, lname: str, email: str):
         self.fname = fname
         self.lname = lname
-        self.fname = fname
+        self.email = email
     def __str__(self):
         return f"{self.fname}, {self.lname}, {self.email}"
 
